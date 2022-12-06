d1541e1330102a2232ec68eaf288de04393c630e
skneo <satishkushwahdigital@gmail.com>
2022-12-06 22:44:05 +0530

    href link corrected

--- resources/views/allArticles.blade.php
+++ resources/views/allArticles.blade.php
@@ -10,13 +10,13 @@
     @if(session()->has('username'))
         @if($category)
         <a href="/new-article/{{$category}}" class="btn btn-outline-primary btn-sm mx-2 mt-2">New</a>
-        @else 
+        @else   
         <a href="/new-article/General" class="btn btn-outline-primary btn-sm mx-2 mt-2">New</a>
         @endif
     @endif
     <ul>
     @foreach ($articles as $article)
-        <li><a class="fs-4" href="article/{{str_replace(' ','-',$article->title)}}/{{$article->id}}">{{$article->title}}</a></li>
+        <li><a class="fs-4" href="/article/{{str_replace(' ','-',$article->title)}}/{{$article->id}}">{{$article->title}}</a></li>
     @endforeach
     </ul>
 </div>
