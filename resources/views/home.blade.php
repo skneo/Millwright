46d001cc9151ebd0ac7cdbfd8e1432ec4df517b7
skneo <satishkushwahdigital@gmail.com>
2022-12-08 22:27:12 +0530

    2:3 image  ratio set

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@ Home
     </div>
     <div class="row">
         <div class="col-md-3 mb-3 mt-3">
-            <div class="card h-100">
+            <div class="card">
                 <div class="card-body text-center">
                     <h5 class="card-title ">M&Ps</h5>
                     <img src="images/machines.jpg" class="card-img-top" alt="machines">
@@ -23,7 +23,7 @@ Home
             </div>
         </div>
         <div class="col-md-3 mb-3 mt-3">
-            <div class="card h-100">
+            <div class="card">
                 <div class="card-body text-center">
                     <h5 class="card-title">Faults</h5>
                     <img src="images/faults.jpg" class="card-img-top" alt="faults">
@@ -33,7 +33,7 @@ Home
             </div>
         </div>
         <div class="col-md-3 mb-3 mt-3">
-            <div class="card h-100">
+            <div class="card">
                 <div class="card-body text-center">
                     <h5 class="card-title ">Spares</h5>
                     <img src="images/spares.png" class="card-img-top" alt="spares">
@@ -43,7 +43,7 @@ Home
             </div>
         </div>
         <div class="col-md-3 mb-3 mt-3">
-            <div class="card h-100">
+            <div class="card">
                 <div class="card-body text-center">
                     <h5 class="card-title ">Articles</h5>
                     <img src="images/articles.jpg" class="card-img-top" alt="articles">
